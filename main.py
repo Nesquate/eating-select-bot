db647f57f65761578bfb3d60ce62e1577be9f9a5
Nesquate <nesquate.100001@gmail.com>
2022-11-25 09:46:05 +0800

    chore: 新增 & 設定 python-dotenv
    稍微重構了一下整體程式碼

--- main.py
+++ main.py
@@ -1,20 +1,33 @@
 import discord
+import os
 from discord.ext import commands
+from dotenv import load_dotenv
 
-TOKEN = ""
 
-intents = discord.Intents.default()
-intents.message_content = True
+def main():
+    intents = discord.Intents.default()
+    intents.message_content = True
 
-bot = commands.Bot(command_prefix='?', intents=intents)
+    bot = commands.Bot(command_prefix='?', intents=intents)
 
-@bot.event
-async def on_ready():
-    print(f"Logged in as {bot.user} (ID: {bot.user.id})")
-    print("-----")
+    @bot.event
+    async def on_ready():
+        print(f"Logged in as {bot.user} (ID: {bot.user.id})")
+        print("-----")
 
-@bot.command()
-async def eat(ctx: commands.Context):
-    await ctx.send("吃東西!")
+    @bot.command()
+    async def eat(ctx: commands.Context):
+        await ctx.send("吃東西!")
 
-bot.run(token=TOKEN)
+    load_dotenv()
+    
+    if os.getenv("BOT_TOKEN") != None:
+        TOKEN =  os.getenv("BOT_TOKEN")
+        bot.run(token=TOKEN)
+    else:
+        print("缺少\"BOT_TOKEN\"環境變數，無法啟動! ")
+
+
+if __name__ == "__main__":
+    main()
+
