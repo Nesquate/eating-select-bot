fa08278e414dce6204589a15d514ba411eefcf63
Nesquate <nesquate.100001@gmail.com>
2022-12-02 14:12:12 +0800

    feature: 按鈕上新增 Emoji

--- views.py
+++ views.py
@@ -9,12 +9,22 @@ class EatWhatView(discord.ui.View):
 
         # self.add_item(discord.ui.Button(label="測試按鈕"))
 
-    @discord.ui.button(label="地圖")
+    # TODO: 按下特定按鈕時要會紀錄喜好值
+
+    @discord.ui.button(label="地圖", emoji="🗺️")
     async def map(self, interation: discord.Interaction, button: discord.ui.Button):
         embed = mapEmbed()
         await interation.response.send_message(embed=embed)
 
-    @discord.ui.button(label="菜單")
+    @discord.ui.button(label="菜單", emoji="📰")
     async def menu(self, interation: discord.Interaction, button: discord.ui.Button):
         embed = menuEmbed()
         await interation.response.send_message(embed=embed)
+
+    @discord.ui.button(emoji="👍")
+    async def like(self, interation: discord.Interaction, button: discord.ui.Button):
+        await interation.response.send_message(content="感謝您的意見，往後將會推薦類似商家")
+
+    @discord.ui.button(emoji="👎")
+    async def dislike(self, interation: discord.Interaction, button: discord.ui.Button):
+        await interation.response.send_message(content="感謝您的意見，下次將不會推薦類似商家")
