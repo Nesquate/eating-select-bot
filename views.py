92a4bf055ee8a29767810ce4e17a576a4a3279af
Nesquate <nesquate.100001@gmail.com>
2023-01-02 14:44:13 +0800

    feat: 暫時停用地圖以及菜單按鈕
    有待爬蟲改善或替代方案成形後再實做

--- views.py
+++ views.py
@@ -10,16 +10,17 @@ class EatWhatView(discord.ui.View):
         # self.add_item(discord.ui.Button(label="測試按鈕"))
 
     # TODO: 按下特定按鈕時要會紀錄喜好值
+    # TODO: 完善爬蟲後搞定地圖與菜單
 
-    @discord.ui.button(label="地圖", emoji="🗺️")
-    async def map(self, interation: discord.Interaction, button: discord.ui.Button):
-        embed = mapEmbed()
-        await interation.response.send_message(embed=embed)
+    # @discord.ui.button(label="地圖", emoji="🗺️")
+    # async def map(self, interation: discord.Interaction, button: discord.ui.Button):
+    #     embed = mapEmbed()
+    #     await interation.response.send_message(embed=embed)
 
-    @discord.ui.button(label="菜單", emoji="📰")
-    async def menu(self, interation: discord.Interaction, button: discord.ui.Button):
-        embed = menuEmbed()
-        await interation.response.send_message(embed=embed)
+    # @discord.ui.button(label="菜單", emoji="📰")
+    # async def menu(self, interation: discord.Interaction, button: discord.ui.Button):
+    #     embed = menuEmbed()
+    #     await interation.response.send_message(embed=embed)
 
     @discord.ui.button(emoji="👍")
     async def like(self, interation: discord.Interaction, button: discord.ui.Button):
